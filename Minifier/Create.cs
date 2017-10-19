adab6afd30cc19057798a82f920b631b4d73415e
DESKTOP-JAN\jan <jan@jan-v.nl>
2017-10-19 19:39:28 +0200

    added imperative binding for the create method

--- Minifier/Create.cs
+++ Minifier/Create.cs
@@ -1,5 +1,7 @@
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -12,16 +14,27 @@ namespace Minifier
     public static class Create
     {
         [FunctionName("Create")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "create")]HttpRequestMessage req, 
-            [DocumentDB("TablesDB", "minified-urls", ConnectionStringSetting = "Minified_ConnectionString", CreateIfNotExists = true)] out MinifiedUrl minifiedUrl,
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "create")]HttpRequestMessage req, 
+            Binder binder,
             TraceWriter log)
         {
             string jsonContent = req.Content.ReadAsStringAsync().Result;
             var data = JsonConvert.DeserializeObject<MinifiedUrl>(jsonContent);
+            var secret = new Secret();
+            var connectionString = await secret.Get("CosmosConnectionStringSecret");
+            ConfigurationManager.AppSettings["CosmosConnectionString"] = connectionString;
+
+            var output = await binder.BindAsync<IAsyncCollector<MinifiedUrl>>(new DocumentDBAttribute("TablesDB", "minified-urls")
+            {
+                CreateIfNotExists = true,
+                ConnectionStringSetting = "CosmosConnectionString",
+            });
 
             var create = new CreateUrlHandler();
-            minifiedUrl = create.Execute(data);
+            var minifiedUrl = create.Execute(data);
 
+            await output.AddAsync(minifiedUrl);
+            
             return req.CreateResponse(HttpStatusCode.Created, $"api/{data.MinifiedSlug}");
         }
     }
