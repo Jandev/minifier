c216b0b4d1ca6857cc1f9d41cd5e0e38871f6986
Jan de Vries <jan@jan-v.nl>
2018-02-14 20:50:53 +0100

    using SqlQuery in DocumentDBAttribute to retrieve object

--- Minifier/Get.cs
+++ Minifier/Get.cs
@@ -1,33 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Minifier.Business;
+using Minifier.Model;
 
 namespace Minifier
 {
     public static class Get
     {
         [FunctionName("Get")]
-        public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{slug}")] HttpRequestMessage req, string slug,
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{slug}")]
+            HttpRequestMessage req, 
+            string slug,
+            [DocumentDB("TablesDB", 
+                "minified-urls", 
+                ConnectionStringSetting = "CosmosConnectionString", 
+                SqlQuery = "SELECT * FROM c WHERE c.MinifiedSlug = {slug}")]
+            IEnumerable<MinifiedUrl> minifiedUrl,
             TraceWriter log)
         {
             log.Info($"Entering the function! Slug is {slug}");
 
-            var getUrl = new GetUrlHandler();
-
-            var minifiedUrl = await getUrl.Execute(slug);
-            if (minifiedUrl == null)
+            if (!minifiedUrl.Any())
             {
-                return req.CreateErrorResponse(HttpStatusCode.NotFound, $"Minified value `{slug}` is not found.");
+                return req.CreateResponse(HttpStatusCode.NotFound);
             }
+
+            var retrievedMinifiedUrl = GetMinifiedUrl(slug, minifiedUrl, log);
+
             var response = req.CreateResponse(HttpStatusCode.Redirect);
-            response.Headers.Location = new Uri(minifiedUrl.FullUrl);
+            response.Headers.Location = new Uri(retrievedMinifiedUrl.FullUrl);
             return response;
+        }
+
+        private static MinifiedUrl GetMinifiedUrl(string slug, IEnumerable<MinifiedUrl> minifiedUrl, TraceWriter log)
+        {
+            if (minifiedUrl.Count() > 1)
+            {
+                log.Warning($"Slug `{slug}` has multiple entries.");
+                return minifiedUrl.Last();
+            }
+
+            return minifiedUrl.First();
         }
     }
 }
