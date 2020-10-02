4a0cf2a3654d3460c7f91f87358bc7271a4f0cf7
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2020-10-02 11:38:21 +0200

    Setting up the Delete action

---
+++ Minifier/Business/IDeleteUrlHandler.cs
@@ -0,0 +1,14 @@
+﻿using Minifier.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minifier.Business
+{
+    internal interface IDeleteUrlHandler
+    {
+        Task<bool> Execute(MinifiedUrl minifiedUrl);
+    }
+}
