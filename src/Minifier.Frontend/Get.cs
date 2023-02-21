638f4e673ecda3b839976a8a8699e038ccc0458d
Jan de Vries <jan@jan-v.nl>
2023-02-21 20:50:17 +0100

    Added update local cache function

--- src/Minifier.Frontend/Get.cs
+++ src/Minifier.Frontend/Get.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Minifier.Frontend
@@ -10,6 +11,8 @@ namespace Minifier.Frontend
     public class Get
     {
         private readonly ILogger<Get> logger;
+
+        private static Dictionary<string, string> _localCache = new Dictionary<string, string>();
 
         public Get(ILogger<Get> logger)
         {
@@ -30,7 +33,17 @@ namespace Minifier.Frontend
         {
             return new RedirectResult(existingMinifiedUrlEntity.url);
         }
-        
+
+        [FunctionName(nameof(UpdateLocalCache))]
+        public void UpdateLocalCache(
+            [ServiceBusTrigger("%IncomingUrlsTopicName%", "%UpdateFrontendSubscription%", Connection = "MinifierIncomingMessages")]
+            MinifiedUrl incomingCreateMinifiedUrlCommand
+            )
+        {
+            _localCache[incomingCreateMinifiedUrlCommand.Slug] = incomingCreateMinifiedUrlCommand.Url;
+            this.logger.LogInformation("Upserted {slug} with {url} to the local cache.", incomingCreateMinifiedUrlCommand.Slug, incomingCreateMinifiedUrlCommand.Url);
+        }
+
         /// <summary>
         /// Lowercasing this property, because Cosmos DB is case sensitive about properties
         /// and using the output binding, like in this Azure Function, doesn't work appear
@@ -39,6 +52,14 @@ namespace Minifier.Frontend
         public class MinifiedUrlEntity
         {
             public string url { get; set; }
+        }
+
+        public class MinifiedUrl
+        {
+            [JsonPropertyName("slug")]
+            public string Slug { get; set; }
+            [JsonPropertyName("url")]
+            public string Url { get; set; }
         }
     }
 }
