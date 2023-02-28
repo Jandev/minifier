74125211554fae561260ab7dd6cf933ab912a36c
Jan de Vries <jan@jan-v.nl>
2023-02-28 22:04:10 +0100

    Using Cosmos DB Client and Cache to find minified urls

--- src/Minifier.Frontend/Get.cs
+++ src/Minifier.Frontend/Get.cs
@@ -11,87 +11,105 @@ using System.Threading.Tasks;
 
 namespace Minifier.Frontend
 {
-    public class Get
-    {
-        private readonly Configuration configuration;
-        private readonly ILogger<Get> logger;
+	public class Get
+	{
+		private readonly Configuration configuration;
+		private readonly ILogger<Get> logger;
 
-        private static Dictionary<string, string> _localCache = new Dictionary<string, string>();
+		private static Dictionary<string, string> localCache = new Dictionary<string, string>();
 
-        public Get(
-            ILogger<Get> logger)
-        {
-            this.configuration = new Configuration();
-            this.logger = logger;
-        }
+		public Get(
+			ILogger<Get> logger)
+		{
+			this.configuration = new Configuration();
+			this.logger = logger;
+		}
 
-        [FunctionName(nameof(Get))]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{slug}")]
-            HttpRequest req,
-            string slug,
-            [CosmosDB(
-                databaseName: "%UrlMinifierRepository:DatabaseName%",
-                containerName: "%UrlMinifierRepository:CollectionName%",
-                Connection = "UrlMinifierRepository"
-                )]
-            CosmosClient client)
-        {
-            var container = client.GetContainer(
-                                    this.configuration.UrlMinifierRepository.DatabaseName, 
-                                    this.configuration.UrlMinifierRepository.CollectionName);
+		[FunctionName(nameof(Get))]
+		public async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{slug}")]
+			HttpRequest req,
+			string slug,
+			[CosmosDB(
+				databaseName: "%UrlMinifierRepository:DatabaseName%",
+				containerName: "%UrlMinifierRepository:CollectionName%",
+				Connection = "UrlMinifierRepository"
+				)]
+			CosmosClient client)
+		{
+			string foundMinifiedUrl = default(string);
+			if (localCache.ContainsKey(slug))
+			{
+				foundMinifiedUrl = Get.localCache[slug];
+				this.logger.LogInformation("Retrieved `{slug}` from cache.", slug);
+			}
+			else
+			{
+				foundMinifiedUrl = await FindUrlFromRepository(slug, client);
+				localCache[slug] = foundMinifiedUrl;
+				this.logger.LogInformation("Added `{slug}` to cache.", slug);
+			}
 
-            var queryDefinition = new QueryDefinition(
-                "SELECT u.url FROM urls u WHERE u.id = @slug")
-                .WithParameter("@slug", slug);
-            var queryRequestOptions = new QueryRequestOptions { PartitionKey = new PartitionKey(slug) };
+			if (foundMinifiedUrl == null)
+			{
+				return new NotFoundResult();
+			}
+			return new RedirectResult(foundMinifiedUrl);
+		}
 
-            string foundMinifiedUrl = default;
-            using (var resultSet = container.GetItemQueryIterator<MinifiedUrlEntity>(
-                                                                    queryDefinition, 
-                                                                    requestOptions: queryRequestOptions)) 
-            {
-                while (resultSet.HasMoreResults) 
-                {
-                    var response = await resultSet.ReadNextAsync();
-                    foundMinifiedUrl = response.First()?.url;
-                    break;
-                }
-            }
+		private async Task<string> FindUrlFromRepository(string slug, CosmosClient client)
+		{
+			var container = client.GetContainer(
+												this.configuration.UrlMinifierRepository.DatabaseName,
+												this.configuration.UrlMinifierRepository.CollectionName);
 
-            if(foundMinifiedUrl == null)
-            {
-                return new NotFoundResult();
-            }
-            return new RedirectResult(foundMinifiedUrl);
-        }
+			var queryDefinition = new QueryDefinition(
+				"SELECT u.url FROM urls u WHERE u.id = @slug")
+				.WithParameter("@slug", slug);
+			var queryRequestOptions = new QueryRequestOptions { PartitionKey = new PartitionKey(slug) };
 
-        [FunctionName(nameof(UpdateLocalCache))]
-        public void UpdateLocalCache(
-            [ServiceBusTrigger("%IncomingUrlsTopicName%", "%IncomingUrlsProcessingSubscription%", Connection = "MinifierIncomingMessages")]
-            MinifiedUrl incomingCreateMinifiedUrlCommand
-            )
-        {
-            _localCache[incomingCreateMinifiedUrlCommand.Slug] = incomingCreateMinifiedUrlCommand.Url;
-            this.logger.LogInformation("Upserted {slug} with {url} to the local cache.", incomingCreateMinifiedUrlCommand.Slug, incomingCreateMinifiedUrlCommand.Url);
-        }
+			string foundMinifiedUrl = default;
+			using (var resultSet = container.GetItemQueryIterator<MinifiedUrlEntity>(
+																	queryDefinition,
+																	requestOptions: queryRequestOptions))
+			{
+				while (resultSet.HasMoreResults)
+				{
+					var response = await resultSet.ReadNextAsync();
+					foundMinifiedUrl = response.First()?.url;
+					break;
+				}
+			}
 
-        /// <summary>
-        /// Lowercasing this property, because Cosmos DB is case sensitive about properties
-        /// and using the output binding, like in this Azure Function, doesn't work appear
-        /// to work with <see cref="JsonPropertyNameAttribute"/> definitions.
-        /// </summary>
-        public class MinifiedUrlEntity
-        {
-            public string url { get; set; }
-        }
+			return foundMinifiedUrl;
+		}
 
-        public class MinifiedUrl
-        {
-            [JsonPropertyName("slug")]
-            public string Slug { get; set; }
-            [JsonPropertyName("url")]
-            public string Url { get; set; }
-        }
-    }
+		[FunctionName(nameof(UpdateLocalCache))]
+		public void UpdateLocalCache(
+			[ServiceBusTrigger("%IncomingUrlsTopicName%", "%IncomingUrlsProcessingSubscription%", Connection = "MinifierIncomingMessages")]
+			MinifiedUrl incomingCreateMinifiedUrlCommand
+			)
+		{
+			localCache[incomingCreateMinifiedUrlCommand.Slug] = incomingCreateMinifiedUrlCommand.Url;
+			this.logger.LogInformation("Upserted {slug} with {url} to the local cache.", incomingCreateMinifiedUrlCommand.Slug, incomingCreateMinifiedUrlCommand.Url);
+		}
+
+		/// <summary>
+		/// Lowercasing this property, because Cosmos DB is case sensitive about properties
+		/// and using the output binding, like in this Azure Function, doesn't work appear
+		/// to work with <see cref="JsonPropertyNameAttribute"/> definitions.
+		/// </summary>
+		public class MinifiedUrlEntity
+		{
+			public string url { get; set; }
+		}
+
+		public class MinifiedUrl
+		{
+			[JsonPropertyName("slug")]
+			public string Slug { get; set; }
+			[JsonPropertyName("url")]
+			public string Url { get; set; }
+		}
+	}
 }
